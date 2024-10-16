f6c9f1fd22c03f3fb15f5da9fbdb51016d3d0863
Thibault Garcia <thibault.garcia.pro@gmail.com>
2024-10-16 16:24:57 +0200

    add book form

--- src/app/pages/library/library.component.ts
+++ src/app/pages/library/library.component.ts
@@ -2,34 +2,51 @@ import { Component } from '@angular/core';
 import book from '../../utils/types/book.type.js';
 import { EmojiReadPipe } from '../../utils/pipes/emoji-read.pipe.js';
 import { CapitalizeFirstLetterPipe } from '../../utils/pipes/capitalize-first-letter.pipe.js';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-library',
   standalone: true,
-  imports: [EmojiReadPipe, CapitalizeFirstLetterPipe],
+  imports: [EmojiReadPipe, CapitalizeFirstLetterPipe, FormsModule],
   templateUrl: './library.component.html',
   styleUrl: './library.component.css',
 })
 export class LibraryComponent {
   books: book[] = [
     {
-      title: 'Livre 1',
+      title: 'Book 1',
       author: 'Author 1',
       isRead: false,
     },
     {
-      title: 'Livre 2',
+      title: 'Book 2',
       author: 'Author 2',
       isRead: false,
     },
     {
-      title: 'Livre 3',
+      title: 'Book 3',
       author: 'Author 3',
       isRead: false,
     },
   ];
 
-  toggleReadStatus(book: book) {
+  book: book = {
+    title: '',
+    author: '',
+    isRead: false,
+  };
+
+  toggleReadStatus(book: book): void {
     book.isRead = !book.isRead;
+  }
+
+  addBook(): void {
+    this.books.push({
+      title: this.book.title,
+      author: this.book.author,
+      isRead: false,
+    });
+    this.book.title = '';
+    this.book.author = '';
   }
 }
